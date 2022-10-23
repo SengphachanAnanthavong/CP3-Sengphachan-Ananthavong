51b2c4c0958708356f4b5845dbd6d7713f5484be
SengphachanAnanthavong <116430355+SengphachanAnanthavong@users.noreply.github.com>
2022-10-23 20:42:13 +0700

    Create Exercise4_Sengphachan_A.py

---
+++ Exercise4_Sengphachan_A.py
@@ -0,0 +1,10 @@
+Foundation_English_Socre = 100
+General_Business_Score = 85.5
+Introduction_to_Computer_Systems_Score=95
+Computer_Programming_Score= 88
+
+print("--- Your Score ---")
+print("Foundation English :",Foundation_English_Socre)
+print("General Business :",General_Business_Score)
+print("Introduction to Computer Systems :",Introduction_to_Computer_Systems_Score)
+print("Computer Programming : ",Computer_Programming_Score)
