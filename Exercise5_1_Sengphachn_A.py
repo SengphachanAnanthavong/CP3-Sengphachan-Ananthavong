0835fa43f8c0455e1384ec6c30aab1047223695e
SengphachanAnanthavong <116430355+SengphachanAnanthavong@users.noreply.github.com>
2022-10-24 16:16:08 +0700

    exercise

---
+++ Exercise5_1_Sengphachn_A.py
@@ -0,0 +1,6 @@
+num1 = 22
+num2 = 12
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
---
+++ Exercise5_2_Sengphachan_A.py
@@ -0,0 +1,7 @@
+from turtle import speed
+
+
+Distance= int(input("Enter distance (km) = "))
+Time = int(input("Enter time (h) : "))
+Speed = Distance/Time
+print(Speed,"km/h")
